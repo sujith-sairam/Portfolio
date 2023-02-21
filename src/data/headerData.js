52e684aed90d3177997018197f978d7aec0dc43e
sujith-sairam <89727004+sujith-sairam@users.noreply.github.com>
2023-02-22 00:15:55 +0530

    social data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ export const headerData = {
     name: 'Sujith Sairam',
     title: "Hello! I am",
     desciption: "Hello! I am Sujith Sairam, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://lh3.googleusercontent.com/p/AF1QipPFw5nrwcsK4JFvkvztYTUYAlCuVBu5ZbAKkySK=w1080-h608-p-no-v0',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqCMGVvXLxmLGeFK4uc_5RmaYyQrOLTH4DMymi-E7k0THCUs9Y',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: 'https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing'
 }
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,9 +1,9 @@
 export const socialsData = {
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    medium: 'https://medium.com/@abusaid-bd',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    mail: 'abusaid7388@gmail.com',
+    github: 'https://github.com/sujith-sairam',
+    facebook: '',
+    linkedIn: 'https://www.linkedin.com/in/sujith-sairam-m-b-e-cse-00ab02218',
+    twitter: 'https://twitter.com/SairamSujith?t=B-IlZfm85h-_ttWpgo-1eQ&s=09',
+    medium: '',
+    stackOverflow: '',
+    mail: 'saisujith589@gmail.com',
 }
